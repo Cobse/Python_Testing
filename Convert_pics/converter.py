3b9dec2b46baba904de51e277abe87515c28ea79
Cobse <Fredrik.jacobsen85@gmail.com>
2023-07-01 08:46:52 +0200

    fix

--- Convert_pics/converter.py
+++ Convert_pics/converter.py
@@ -3,13 +3,14 @@ import glob
 import os
 
 path_in = r"E:\GitHub\Python_Testing\Convert_pics\Originals\\"
-path_out = r"E:\GitHub\Python_Testing\Convert_pics\Out\\"
+path_out = r"E:\GitHub\Python_Testing\Convert_pics\Out"
 
 list_pngs = glob.glob(path_in + "*.png")
 print(list_pngs)
 counter = 0
-for image in glob.glob(path_in + "pu*.jpg"):
+for image in glob.glob(path_in + "stat*.jpg"):
     counter = counter + 1
     img = Image.open(image)
     filename = os.path.basename(image)
-    img.save(os.path.join(path_out, filename),'png')
+    filename_ext = os.path.splitext(filename)
+    img.save(os.path.join(path_out, filename_ext[0] + ".png"),'png')
