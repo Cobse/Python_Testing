f442ab935dde5b41d6464af69329344afd04752a
Cobse <Fredrik.jacobsen85@gmail.com>
2023-07-05 08:12:27 +0200

    converter

--- Convert_pics/converter.py
+++ Convert_pics/converter.py
@@ -2,15 +2,16 @@ from PIL import Image
 import glob
 import os
 
-path_in = r"E:\GitHub\Python_Testing\Convert_pics\Originals\\"
+path_in = r"E:\GitHub\Python_Testing\Convert_pics\Input\\"
 path_out = r"E:\GitHub\Python_Testing\Convert_pics\Out"
 
-list_pngs = glob.glob(path_in + "*.png")
-print(list_pngs)
-counter = 0
-for image in glob.glob(path_in + "stat*.jpg"):
-    counter = counter + 1
+list_pngs = glob.glob(path_in + "*.jpg")
+print(*list_pngs,sep="\n")
+
+for image in glob.glob(path_in + "*.png"):
     img = Image.open(image)
     filename = os.path.basename(image)
     filename_ext = os.path.splitext(filename)
     img.save(os.path.join(path_out, filename_ext[0] + ".png"),'png')
+
+print("finished")
