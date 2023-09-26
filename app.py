cf069d2261c162173e85e1040afd454432c89aed
Cobse <Fredrik.jacobsen85@gmail.com>
2023-09-26 14:33:49 +0200

    small example

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.set_page_config(page_title="My webpage", page_icon=":tada", layout="wide")
 
@@ -7,3 +9,9 @@ st.subheader("Hi dude!")
 st.title("This is the Title")
 st.write("I'm AWESOME!")
 st.write("I'm AWESOME again!")
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
