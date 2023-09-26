0fa054df2f2ecfc833e90f23bd24cf4e57d43086
Cobse <Fredrik.jacobsen85@gmail.com>
2023-09-26 14:23:24 +0200

    added streamlit app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="My webpage", page_icon=":tada", layout="wide")
+
+# ----- Header Section ----
+st.subheader("Hi dude!")
+st.title("This is the Title")
+st.write("I'm AWESOME!")
+st.write("I'm AWESOME again!")
