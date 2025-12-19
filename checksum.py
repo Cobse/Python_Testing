53a9e69386abcff425a49adf8b16aa55ae135e0e
Cobse <Fredrik.jacobsen85@gmail.com>
2025-12-19 16:05:13 +0100

    checksum examples

--- checksum.py
+++ checksum.py
@@ -21,6 +21,13 @@ def calculate_checksum(bvbs_without_checksum: str) -> str:
 
 
 input_string = "BF2D@Hj123@rCP-32@i0@pG1-01-2/1500.1@l6300@n6@e9.94@d16@gB500NC@s50@a0@t0@Gl6300@w0@C"
+# Curved rebar test case
+input_string = "BF2D@HjTestPDF@r417@ia@p1@l12000@n10@e1.268@d12@gB500A@s48@v@Gl100@w0@r8402.5@w81.82676@w100@l0@w0@C"
+# Curved rebar test case with different length
+input_string = "BF2D@r8082@HjA267920@w85@rCP-0(?)@i0@pF4-1/1000@l12000@n3@e46.24@d25@gB500NC@s125@a0@t0@Gl0@r8082@w85@C"
+
+#input_string = "BF2D@Hj123@rCP-1135(?)@i0@pOb1100@l5000@n128@e19.28@d25@gB500NC@s125@a0@t0@Gl375@w90@l3920@w180@l215@w0@C"
+
 
 # Example usage
 checksum = calculate_checksum(input_string)
