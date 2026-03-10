a7236e0accf92b28cd90c126b652fd2b4a0c5a75
Cobse <Fredrik.jacobsen85@gmail.com>
2026-03-10 11:21:41 +0100

    zed settings

--- checksum.py
+++ checksum.py
@@ -1,22 +1,19 @@
-
-
-
 def calculate_checksum(bvbs_without_checksum: str) -> str:
     """
     Calculate checksum for BVBS line using ASCII values.
-    
+
     Args:
         bvbs_without_checksum (str): Input string without checksum
-    
+
     Returns:
         str: Calculated checksum as string
     """
     # Convert string to ASCII bytes and sum them
     sum_bytes = sum(ord(char) for char in bvbs_without_checksum)
-    
+
     # Calculate checksum: 96 - (sum modulo 32)
     checksum = 96 - (sum_bytes % 32)
-    
+
     return str(checksum)
 
 
@@ -27,9 +24,11 @@ input_string = "BF2D@HjTestPDF@r417@ia@p1@l12000@n10@e1.268@d12@gB500A@s48@v@Gl1
 # input_string = "BF2D@Hj123@rCP-1139(?)@i0@pOc2000@l9760@n92@e37.61@d25@gB500NC@s0@a0@t0@Gl9760@w0@C88@"
 input_string = "BF2D@Hj123@rCP-1139(?)@i0@pOc2000@l9760@n92@e37.61@d25@gB500NC@s0@a0@t0@Gl9760@w0@C"
 
-#input_string = "BF2D@Hj123@rCP-1135(?)@i0@pOb1100@l5000@n128@e19.28@d25@gB500NC@s125@a0@t0@Gl375@w90@l3920@w180@l215@w0@C"
+# input_string = "BF2D@Hj123@rCP-1135(?)@i0@pOb1100@l5000@n128@e19.28@d25@gB500NC@s125@a0@t0@Gl375@w90@l3920@w180@l215@w0@C"
 
 
 # Example usage
 checksum = calculate_checksum(input_string)
-print(f"Checksum for '{input_string}' with length: '{len(input_string)}'  is: {checksum}")
+print(
+    f"Checksum for '{input_string}' with length: '{len(input_string)}'  is: {checksum}"
+)
